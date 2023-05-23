d4f3c88295cf01ebc75698fc7dadc954ef473088
Troy Mathis <troy.mathis00@gmail.com>
2023-05-23 11:03:04 -0500

    initialized movie search component, started api connection

---
+++ app/components/MovieSearch.tsx
@@ -0,0 +1,14 @@
+
+"use client"
+import {useState} from 'react'
+
+const MovieSearch = () => {
+    const [movie,setMovies] = useState([]);
+    
+
+  return (
+    <div>MovieSearch</div>
+  )
+}
+
+export default MovieSearch;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
+import Head from "next/head"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
-    </main>
+    <div className="bg-black min-h-screen">
+      <Head>
+        <title>Movie Database</title>
+        <meta name="description" content="A simple movie database for users" />
+      </Head>
+      <main className="container mx-auto py-10 px-4 flex flex-col items-center justify-center">
+        <h1 className="text-4x1 font-bold mb-8 text-white">Movie Database</h1>
+        
+      </main>
+    </div>
   )
 }
