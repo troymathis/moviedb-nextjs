15c69a38c6d47e79e74156fad55bbe7081956af0
Troy Mathis <troy.mathis00@gmail.com>
2023-05-23 10:46:29 -0500

    initialized

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: ['400', '600'],
+  subsets: ['latin']
+})
+
+export const metadata = {
+  title: 'Movie Database',
+  description: 'Movie Datavase',
+  keywords: ['movie, database']
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
